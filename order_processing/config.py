62fb829b30796ca7cc14fac762fa9441768ae394
Prerna2415 <pskunder2005@gmail.com>
2026-08-25 08:04:18 +0530

    Improve environment configuration

--- order_processing/config.py
+++ order_processing/config.py
@@ -2,5 +2,5 @@ import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///orders.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your_secret_key')
+JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
 PRODUCT_SERVICE_URL = os.environ.get('PRODUCT_SERVICE_URL', 'http://localhost:9002')
